f26ee5187a52d71340e26489b80e44c330f659a6
angelle rina <angellerinac@gmail.com>
2020-09-01 14:14:33 -0400

    Todo Item Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 class App extends Component {
@@ -37,7 +38,7 @@ class App extends Component {
     console.log("clear list ");
   };
   handleDelete = (id) => {
-    console.log(`handle edit ${id}`);
+    console.log(`handle delete ${id}`);
   };
   handleEdit = (id) => {
     console.log(`edit edit ${id}`);
